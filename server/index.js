2800ab711a128a0a3da5cbf2259e5ba76468caf0
odinarussell@gmail.com <odinarussel@gmail.com>
2023-01-09 13:39:56 -0600

    tested openai api.. test works

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
-import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   organization: "org-Sv5ywSdJVQU0pVE97V3fElmC",
-  apiKey: "sk - aKjEIlE6Fc86jFHgLdh1T3BlbkFJXQJm7d92HER2Rtsa9pAJ",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
+
+async function callApi() {
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: "Say this is a test",
+    max_tokens: 7,
+    temperature: 0,
+  });
+  console.log(response.data.choices[0].text)
+}
+
+callApi()
