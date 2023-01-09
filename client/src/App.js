3cdcce34f96b9f928a61a761c7e7480197b7c0eb
odinarussell@gmail.com <odinarussel@gmail.com>
2023-01-09 10:32:10 -0600

    building out message questionaire

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,22 @@ function App() {
         </h1>
       </aside>
       <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="avatar">
+              Me
+            </div>
+            <div className="message">
+              Hello World
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea
+            rows="1"
+            className="chat-input-textarea">
+          </textarea>
+        </div>
 
       </section>
     </div>
